75aaa5d4013569758134db36900b0ca7b2ad859b
My Repo <shubhamarya35678@gmail.com>
2026-02-08 00:21:53 +0530

    Update userbot.py

--- anony/core/userbot.py
+++ anony/core/userbot.py
@@ -59,7 +59,7 @@ class Userbot(Client):
         client.mention = ub.me.mention
         self.clients.append(client)
         try:
-            await ub.join_chat("DebugAngels")
+            await ub.join_chat("TgMusicBots")
         except:
             pass
         logger.info(f"Assistant {num} started as @{client.username}")
