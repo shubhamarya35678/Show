ed5ce5da690fac43ca50508dd0eeedd0ee993c3c
My Repo <shubhamarya35678@gmail.com>
2026-02-08 13:56:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,8 +33,8 @@ class Config:
             if url and "batbin.me" in url
         ]
         self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://te.legra.ph/file/3e40a408286d4eda24191.jpg")
-        self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/haagg2.png")
-        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/zvziwk.jpg")
+        self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/uqhjdt.jpg")
+        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/tjux0q.jpg")
         self.API_KEY = getenv("API_KEY", None)
         self.API_URL = getenv("API_URL", "https://babyapi.pro")
 
