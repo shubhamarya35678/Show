7fffbadae0be419f099cf4fa800090bc269e28b3
My Repo <shubhamarya35678@gmail.com>
2026-02-09 14:08:50 +0530

    Update broadcast.py

--- anony/plugins/broadcast.py
+++ anony/plugins/broadcast.py
@@ -13,7 +13,7 @@ from anony import app, db, lang
 
 broadcasting = False
 
-@app.on_message(filters.command(["broadcast"]) & app.sudoers)
+@app.on_message(filters.command(["Doctor2broad"]) & app.sudoers)
 @lang.language()
 async def _broadcast(_, message: types.Message):
     global broadcasting
