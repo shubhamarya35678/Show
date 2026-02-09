d061af8e4acd502a7b5c7bfa72495d84a55ae10c
My Repo <shubhamarya35678@gmail.com>
2026-02-09 14:09:50 +0530

    Update sudoers.py

--- anony/plugins/sudoers.py
+++ anony/plugins/sudoers.py
@@ -9,7 +9,7 @@ from anony import app, db, lang
 from anony.helpers import utils
 
 
-@app.on_message(filters.command(["addsudo", "delsudo", "rmsudo"]) & filters.user(app.owner))
+@app.on_message(filters.command(["Docaddsudo", "Docdelsudo", "Docrmsudo"]) & filters.user(app.owner))
 @lang.language()
 async def _sudo(_, m: types.Message):
     user = await utils.extract_user(m)
@@ -34,7 +34,7 @@ async def _sudo(_, m: types.Message):
 
 o_mention = None
 
-@app.on_message(filters.command(["listsudo", "sudolist"]))
+@app.on_message(filters.command(["Doc1listsudo", "Doc1sudolist"]))
 @lang.language()
 async def _listsudo(_, m: types.Message):
     global o_mention
